70ad81e83a2ba134b32526c62762ccb5b686661d
Sujit Banne <sujitbanne13@gmail.com>
2023-03-08 12:39:25 +0530

    first Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from 'react';
+import axios from 'axios'
+import './index.css';
 function App() {
+
+  const [data, setData] = useState({})
+  const [location, setLocation] = useState('')
+
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios
+        .get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=5b4fbcf7d6e509eb53c9da99b0420765`)
+        .then((response) => {
+          console.log(response.data);
+          setData(response.data)
+        })
+      setLocation('')
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="search">
+        <input
+          value={location}
+          onChange={event => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          placeholder='Enter Location'
+          type="text" />
+      </div>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>{data.name}</p>
+          </div>
+          <div className="temp">
+            {data.main ? <h1>{(data.main.temp * 9 / 5 - 32).toFixed()}°C</h1> : null}
+          </div>
+
+          <div className="description">
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/**
+ * https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+ * 
+ * 5b4fbcf7d6e509eb53c9da99b0420765
+ */
